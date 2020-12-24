c736d1b3bbabf9db2cc54827461a1ff91724fd77
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2020-12-24 20:16:11 +0200

    Added Constructing Instruction Word from OpCode and Operands

--- operands_identifier.py
+++ operands_identifier.py
@@ -1,8 +1,8 @@
 import re
 
 X = A = N = r"[0-9]+"
-Rn = r"R[0-7]"
-Rn_indirect = fr"\({Rn}\)"
+Rn = r"R[0-7]"  # Regex for Registers : R0 , R1 ... R7
+Rn_indirect = fr"\({Rn}\)"  # Regex for indirect Registers : (R0) , (R1) ... (R7)
 
 addressing_modes = {
     "register_direct": {"regex": fr"{Rn}", "size": 0, "code": "000"},
@@ -24,11 +24,52 @@ addressing_modes = {
 }
 
 
+# TODO : Replace Variable Name with its address
+
 def get_addressing_mode(operand: str) -> str:
     for addressing_mode in addressing_modes:
         addressing_mode_regex = addressing_modes[addressing_mode].get('regex')
 
         if re.fullmatch(addressing_mode_regex, operand) is not None:
             return addressing_mode
-    raise AttributeError  # if did not match any addressing mode
+    # raise AttributeError  # if did not match any addressing mode
 
+
+def get_register(operand: str) -> str:
+    addressing_mode = get_addressing_mode(operand)
+    if addressing_mode in ["immediate", "absolute", "relative_direct", "relative_indirect"]:
+        register = "R7"
+    else:
+        register = re.search(Rn, operand).group()
+    return register
+
+
+def get_register_code(register: str) -> str:
+    if register is not None:
+        return format(int(register[1]), '03b')
+
+
+def handle_instruction(address: int, instruction_opcode: str, operand1: str, operand2: str):
+    """
+    Returns list of binary strings represents this instruction
+    First Element is the instruction
+    Next Elements are Data needed (Addresses of Immediate Values) [Optional]
+    """
+    instruction = instruction_opcode
+
+    reg1 = reg2 = None
+    reg1_addressing_mode = reg2_addressing_mode = None
+
+    if operand1 is not None:
+        reg1 = get_register_code(get_register(operand1))
+        reg1_addressing_mode = get_addressing_mode(operand1)
+        reg1_mode_code = addressing_modes[reg1_addressing_mode].get('code')
+        instruction += reg1_mode_code + reg1
+
+    if operand2 is not None:
+        reg2 = get_register_code(get_register(operand2))
+        reg2_addressing_mode = get_addressing_mode(operand2)
+        reg2_mode_code = addressing_modes[reg2_addressing_mode].get('code')
+        instruction += reg2_mode_code + reg2
+
+    return [instruction]
