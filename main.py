7c832f522cf45c6f840fb27178a2a1ce5d96b419
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2020-12-25 02:00:15 +0200

    Added Completed Handling Addressing Modes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
+
 from IO import read_assembly_lines
-from instructions_identifier import instructions_identifier, handle_variable, handle_label, variables_table, labels_table
+from instructions_identifier import *
+from operands_identifier import *
 
 argsNum = len(sys.argv)  # TODO : Checks
 # asmFilePath = sys.argv[1]
@@ -8,7 +10,6 @@ asmFilePath = 'main.asm'
 print(asmFilePath)
 
 assembly_lines = read_assembly_lines(asmFilePath)
-
 
 IR = []
 
@@ -19,4 +20,17 @@ for line in assembly_lines:
     if line['content'].endswith(':'):
         handle_label(IR, line)
         continue
+
+for line in assembly_lines:
+    if line['content'].startswith('DEFINE'):
+        continue
+    if line['content'].endswith(':'):
+        continue
+
     instruction_type, opcode, op1, op2 = instructions_identifier(line)
+
+    if instruction_type == "1op" or instruction_type == "2op":
+        program_counter = len(IR)
+        instruction = handle_instruction(program_counter, opcode, op1, op2)
+        IR += instruction
+        print(line, instruction)
